b8b2f80f28a5194de5bb6bf0ea596faf70a54716
Ela <raziazaman@iut-dhaka.edu>
2025-01-20 14:40:12 +0600

    Migrate Enty Model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Topics(models.Model):
+class Topic(models.Model):
     """A topic the user is learning about"""
     text = models.CharField(max_length = 200)
     date_added = models.DateTimeField(auto_now_add=True)
@@ -9,3 +9,18 @@ class Topics(models.Model):
     def __str__(self):
         """Return a string representation of the model."""
         return self.text
+    
+class Entry(models.Model):
+    """Something specific learned about a topic. """
+    topic = models.ForeignKey(Topic, on_delete = models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add = True)
+
+    class Meta :
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return a simple string representing the entry."""
+        return f"{self.text[:50]}..."
+    
+
