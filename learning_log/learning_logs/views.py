6d5cceb8a82de9a26f4fcaec5a337568e9fef846
Ela <raziazaman@iut-dhaka.edu>
2025-02-10 10:08:50 +0600

    Restricting Access with @login_required

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -8,20 +9,21 @@ from .forms import TopicForm, EntryForm
 def index(request):
     """The home page for Learning Log."""
     return render(request, 'learning_logs/index.html')
-
+@login_required
 def topics(request):
     """Shows all topics."""
     topics = Topic.objects.order_by('date_added')
     context = {'topics' : topics}
     return render(request,'learning_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     """"Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
     entries= topic.entry_set.order_by('-date_added')
     context = {'topic' : topic, 'entries' : entries}
     return render(request, 'learning_logs/topic.html', context)
-
+@login_required
 def new_topic(request):
     """Add new topic."""
 
@@ -37,7 +39,7 @@ def new_topic(request):
     #Display a blank or invalid form.
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
-
+@login_required
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
@@ -56,7 +58,7 @@ def new_entry(request, topic_id):
     #Display a blank or invalid form.
     context = {'topic':topic,'form':form}
     return render(request, 'learning_logs/new_entry.html',context)
-
+@login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
